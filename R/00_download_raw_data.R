df88aef6ac683f9702559ab73d41a6c3e34a88b3
nikpaw <112829804+nikpaw@users.noreply.github.com>
2025-04-25 19:01:07 +0200

    starting to set up a cleaned version

---
+++ R/00_download_raw_data.R
@@ -0,0 +1,18 @@
+fs::dir_create(here::here("data", "raw"))
+
+# Define Dropbox direct download URL for the ZIP file
+
+zip_url <- "https://www.dropbox.com/scl/fi/ia32wnerdasw2bxmhe9lv/raw_data.zip?rlkey=yvim5mogztb5sqe0821rjanqh&st=gd86j19v&dl=1"
+
+# Define local path to store the downloaded ZIP file
+zip_path <- here::here("data", "raw_data.zip")
+
+# Define directory to unzip contents
+unzip_dir <- here::here("data", "raw")
+
+# Download and unzip if not already present
+if (!fs::file_exists(zip_path)) {
+  httr::GET(zip_url, httr::write_disk(zip_path, overwrite = TRUE))
+  utils::unzip(zipfile = zip_path, exdir = unzip_dir)
+  fs::file_delete(zip_path)
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MDS_MT_clean
 Data pipeline and causal impact study of German hospital closures (2010–2020) on local labor markets
+
+
+## Data Access
+
+Due to size and versioning constraints, raw data is not stored in this repository.
+
+The data can be accessed through the following options this 
+
+Shared institutional folder (link):
+   [TBA](TBA)
+   
+
